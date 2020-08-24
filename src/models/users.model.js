c7c4b507db4a2c3832993833baa68e8573b8dea9
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2020-08-24 05:32:38 +0100

    `feathers generate app` command executed to generate the initial app scaffolding.

---
+++ src/models/users.model.js
@@ -0,0 +1,38 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
+  
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  
+  
+    githubId: { type: DataTypes.STRING },
+  
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  users.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return users;
+};
