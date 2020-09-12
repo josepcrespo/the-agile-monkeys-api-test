de809618f2dd9036dd9c6262f7b417131753ad65
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2020-09-12 19:20:27 +0100

    Basic tests coverage with text summary on the command line.

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -7,4 +7,43 @@ describe('users', () => {
 
     assert.ok(service, 'Registered the service');
   });
+
+  it('creates a user (from inside the app) and, encrypts his password', async () => {
+    // Calling a Feathers service from inside the Feathers app
+    // can be done without providing authentication on the request.
+    const user = await app.service('users').create({
+      //email: 'test' + Math.floor(Math.random() * 1000) + '@example.com',
+      email: 'test' + Math.floor(Math.random() * 1000) + '@example.com',
+      password: 'secret'
+    });
+
+    // Makes sure the password got encrypted
+    assert.ok(user.password !== 'secret');
+  });
+
+  it('removes password for external requests', async () => {
+    // Setting `provider` indicates an external request
+    const params = {
+      provider: 'rest',
+      /* The method provided by the official docs does not work:
+       *
+       * https://docs.feathersjs.com/guides/basics/testing.html#testing-services
+       *
+       * but we can fake we are authenticated,
+       * setting the `authenticated` property to `true`.
+       *
+       * You can check it on the `authenticate.js` module file:
+       * https://docs.feathersjs.com/guides/basics/testing.html#testing-services
+       */
+      authenticated: true
+    };
+
+    const user = await app.service('users').create({
+      email: 'test' + Math.floor(Math.random() * 1000) + '@example.com',
+      password: 'secret'
+    }, params);
+
+    // Make sure password has been removed
+    assert.ok(!user.password);
+  });
 });
